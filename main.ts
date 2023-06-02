ef664d5ac27304f475e90d6cfc178f8badb14203
nuriaramoos <132547909+nuriaramoos@users.noreply.github.com>
2023-06-02 12:38:31 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "POWER") {
+        encendido = !(encendido)
+    }
+})
+let encendido = false
+radio.setGroup(1)
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
